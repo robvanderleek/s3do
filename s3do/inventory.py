80e820a64e4b305d7ed70998f8772cea5464c3dd
Rob van der Leek <robvanderleek@gmail.com>
2021-02-15 23:18:08 +0100

    Log missing credentials

--- s3do/inventory.py
+++ s3do/inventory.py
@@ -1,5 +1,8 @@
+import logging
+
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 
 def get_objects_batch(client, bucket, prefix, token):
@@ -11,13 +14,11 @@ def get_objects_batch(client, bucket, prefix, token):
                                       MaxKeys=1000, Prefix=prefix)
 
 
-def list_objects(bucket, prefix):
-    client = boto3.client('s3')
+def list_objects(s3, bucket, prefix):
     has_more = True
     token = None
     while has_more:
-        object_list = get_objects_batch(client, bucket, prefix,
-                                        token)
+        object_list = get_objects_batch(s3, bucket, prefix, token)
         for o in object_list['Contents']:
             print(f'{bucket},{o["Key"]}')
         has_more = object_list['IsTruncated']
@@ -28,4 +29,9 @@ def list_objects(bucket, prefix):
 @click.argument('bucket')
 @click.argument('prefix', required=False)
 def inventory(bucket, prefix):
-    list_objects(bucket, prefix)
+    logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s')
+    try:
+        client = boto3.client('s3')
+        list_objects(client, bucket, prefix)
+    except ClientError as e:
+        logging.error(e)
